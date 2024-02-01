2e4ee02246fb1a8da0806e7596c4bb8c8ba36158
张云飞 <2980439042@qq.com>
2024-02-01 10:02:28 +0800

    初始化gorm

--- service/chat/internal/svc/servicecontext.go
+++ service/chat/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "Link/service/chat/internal/config"
+import (
+	initGorm "Link/internal/gorm"
+	"Link/service/chat/internal/config"
+	"gorm.io/gorm"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	DB     *gorm.DB
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	db := initGorm.InitGorm(c.Mysql.DataSource)
 	return &ServiceContext{
 		Config: c,
+		DB:     db,
 	}
 }
