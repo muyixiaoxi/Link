6135e323cd536ca24f9d565afb1479e3c24455a8
张云飞 <2980439042@qq.com>
2024-02-18 08:00:28 +0800

    配置跨域,docker-compose

--- restful/tag/tag.go
+++ restful/tag/tag.go
@@ -30,7 +30,7 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c)
 
-	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(jwt.JwtUnauthorizedResult))
+	server := rest.MustNewServer(c.RestConf, rest.WithUnauthorizedCallback(jwt.JwtUnauthorizedResult), rest.WithCors())
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
