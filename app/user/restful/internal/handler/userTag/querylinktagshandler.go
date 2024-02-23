17b28b74e99f46104e64508ba6ce39e38638aa3e
张云飞 <2980439042@qq.com>
2024-02-23 09:20:49 +0800

    查询用户选择标签

--- app/user/restful/internal/handler/userTag/querylinktagshandler.go
+++ app/user/restful/internal/handler/userTag/querylinktagshandler.go
@@ -6,14 +6,21 @@ import (
 	"net/http"
 	"user/common/response"
 
+	"github.com/zeromicro/go-zero/rest/httpx"
 	"user/restful/internal/logic/userTag"
 	"user/restful/internal/svc"
+	"user/restful/internal/types"
 )
 
 func QueryLinkTagsHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.UserInfoRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
 		l := userTag.NewQueryLinkTagsLogic(r.Context(), svcCtx)
-		resp, err := l.QueryLinkTags()
+		resp, err := l.QueryLinkTags(&req)
 		if err != nil {
 			logc.Error(context.Background(), "l.QueryLinkTags() is failed", err)
 			response.Response(w, nil, response.CodeServerBusy)
