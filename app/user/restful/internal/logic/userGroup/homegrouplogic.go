498965857b36ec3c9983d6b02efddb04e7dfb34d
张云飞 <2980439042@qq.com>
2024-03-01 18:13:48 +0800

    修改推荐列表

--- app/user/restful/internal/logic/userGroup/homegrouplogic.go
+++ app/user/restful/internal/logic/userGroup/homegrouplogic.go
@@ -30,32 +30,22 @@ func (l *HomeGroupLogic) HomeGroup(req *types.RecommendGroupByTagRequest) (resp 
 	//获取当前登录用户的id
 	jid := l.ctx.Value("user_id").(json.Number)
 	userId, _ := jid.Int64()
-	var (
-		selfIds   []uint64
-		systemIds []uint64
-	)
+	var tagIds []uint64
 	//如果用户没有选择标签
-	if len(req.UserSelfTagId) == 0 && len(req.SystemTagId) == 0 {
+	if len(req.TagIds) == 0 {
 		//查询出用户自己相关的标签
 		respRpc, _ := l.svcCtx.TagLoginRpc.SelectLinkTags(l.ctx, &tag.SelectLinkTagsRequest{Id: uint64(userId)})
 		for _, value := range respRpc.LinkTags {
-			if value.CreatorId != 0 {
-				//我加入或者创建的标签
-				selfIds = append(selfIds, value.Id)
-			} else {
-				systemIds = append(systemIds, value.Id)
-			}
+			tagIds = append(tagIds, value.Id)
 		}
 	} else {
-		systemIds = append(systemIds, req.SystemTagId...)
-		selfIds = append(selfIds, req.UserSelfTagId...)
+		tagIds = append(tagIds, req.TagIds...)
 	}
 	//查询相关群聊
 	respGroup, err := l.svcCtx.UserRpc.UserSelectGroup(l.ctx, &user.UserSelectGroupsRequest{
-		SystemTagId:   systemIds,
-		UserSelfTagId: selfIds,
-		PageNo:        req.PageNo,
-		PageSize:      req.PageSize,
+		TagId:    tagIds,
+		PageNo:   req.PageNo,
+		PageSize: req.PageSize,
 	})
 	if err != nil {
 		return nil, err
--- app/user/service/internal/logic/recommenduserslogic.go
+++ app/user/service/internal/logic/recommenduserslogic.go
@@ -26,20 +26,7 @@ func NewRecommendUsersLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Re
 func (l *RecommendUsersLogic) RecommendUsers(in *user.RecommendUsersRequest) (resp *user.RecommendUsersResponse, err error) {
 	//推荐人员信息
 	//先查询出符合条件的人员总数
-	if len(in.UserSelfTagId) == 0 {
-		//没有选择用户自定义标签则根据系统标签查询
-		resp, err = QueryUsersByConditions(l.svcCtx.DB, in.PageNo, in.PageSize, in.SystemTagId, in.UserId)
-		if err != nil {
-			return nil, err
-		}
-
-	} else {
-		//选择了用户自定义标签 , 则根据用户自定义标签查询
-		resp, err = QueryUsersByConditions(l.svcCtx.DB, in.PageNo, in.PageSize, in.UserSelfTagId, in.UserId)
-		if err != nil {
-			return nil, err
-		}
-	}
+	resp, err = QueryUsersByConditions(l.svcCtx.DB, in.PageNo, in.PageSize, in.TagId, in.UserId)
 	return resp, err
 }
 
@@ -56,11 +43,11 @@ func QueryUsersByConditions(db *gorm.DB, pageNo, pageSize int64, tagIds []uint64
 		Count(&totalCount).Error; err != nil {
 		return nil, err
 	}
-	if err := db.
+	if err := db.Debug().
 		Table("users").
 		Joins("JOIN tb_user_tag ON users.id = tb_user_tag.user_id AND users.deleted_at IS NULL AND tb_user_tag.deleted_at IS NULL").
 		Joins("JOIN tb_tag ON tb_user_tag.tag_id = tb_tag.id").
-		Where("tb_tag.id IN (?) AND tb_tag.type != 'OFFICIAL' AND users.id != ? AND users.deleted_at IS NULL", tagIds, userID).
+		Where("tb_tag.id IN (?) AND users.id != ? AND users.deleted_at IS NULL", tagIds, userID).
 		Select("users.username, users.avatar, users.signature, users.id").
 		Group("users.id").
 		Offset(int(offset)).
