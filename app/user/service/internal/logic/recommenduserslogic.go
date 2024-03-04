3e21a92ac3c29325dd5e110bb6748b8655624160
张云飞 <2980439042@qq.com>
2024-03-04 21:22:05 +0800

    修改分页参数

--- app/user/service/internal/logic/recommenduserslogic.go
+++ app/user/service/internal/logic/recommenduserslogic.go
@@ -31,19 +31,9 @@ func (l *RecommendUsersLogic) RecommendUsers(in *user.RecommendUsersRequest) (re
 }
 
 func QueryUsersByConditions(db *gorm.DB, pageNo, pageSize int64, tagIds []uint64, userID uint64) (*user.RecommendUsersResponse, error) {
-	var totalCount int64
 	var userList []*user.RecommendUser
 	offset := (pageNo - 1) * pageSize
 	if err := db.
-		Table("users").
-		Joins("JOIN tb_user_tag ON users.id = tb_user_tag.user_id AND users.deleted_at IS NULL AND tb_user_tag.deleted_at IS NULL").
-		Joins("JOIN tb_tag ON tb_user_tag.tag_id = tb_tag.id").
-		Where("tb_tag.id IN (?) AND users.id != ? AND users.deleted_at IS NULL", tagIds, userID).
-		Group("users.id").
-		Count(&totalCount).Error; err != nil {
-		return nil, err
-	}
-	if err := db.Debug().
 		Table("users").
 		Joins("JOIN tb_user_tag ON users.id = tb_user_tag.user_id AND users.deleted_at IS NULL AND tb_user_tag.deleted_at IS NULL").
 		Joins("JOIN tb_tag ON tb_user_tag.tag_id = tb_tag.id").
@@ -55,5 +45,6 @@ func QueryUsersByConditions(db *gorm.DB, pageNo, pageSize int64, tagIds []uint64
 		Find(&userList).Error; err != nil {
 		return nil, err
 	}
-	return &user.RecommendUsersResponse{RecommendUserList: userList, Total: totalCount}, nil
+
+	return &user.RecommendUsersResponse{RecommendUserList: userList, Total: int64(len(userList))}, nil
 }
