70d4a5259e34fa34a3e838b9b8848713bd263a26
张云飞 <2980439042@qq.com>
2024-03-09 16:57:14 +0800

    增加消息类型字段

--- app/user/restful/internal/types/types.go
+++ app/user/restful/internal/types/types.go
@@ -47,6 +47,7 @@ type Message struct {
 	From         uint64 `json:"from,optional"`
 	To           uint64 `json:"to"`
 	Type         uint8  `json:"type"`
+	ContentType  uint8  `json:"contentType"`
 	Time         string `json:"time"`
 	Content      string `json:"content"`
 	FromAvatar   string `json:"fromAvatar"`
